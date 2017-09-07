6dfb166f502bbb6475a6ba7cac71324f8279e36c
ktausch <Keith.Tauscher@colorado.edu>
2017-09-07 16:24:19 -0600

    Started simple README file.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ File: setup.py
 Author: Keith Tauscher
 Date: 25 Aug 2017
 
-Description: Installs extractpy.
+Description: Installs pylinex.
 """
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
         
-setup(name='extractpy', version='0.1',\
-    description='Signal extraction in Python', packages=['extractpy'])
+setup(name='pylinex', version='0.1',\
+    description='Linear signal extraction in Python', packages=['pylinex'])
 
     
     
