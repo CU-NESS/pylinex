2e77e0459bcb58818fc0522dd5ab727319d06d6c
ktausch <Keith.Tauscher@colorado.edu>
2017-09-08 19:51:24 -0600

    Added missing __init__.py file in pylinex package.

---
+++ pylinex/__init__.py
@@ -0,0 +1,24 @@
+"""
+File: pylinex/__init__.py
+Author: Keith Tauscher
+Date: 3 Sep 2017
+
+Description: Imports the most important elements from all submodules. The
+             imports written here are designed to support imports of the form,
+             "from pylinex import ______", where ______ is the name of a
+             desired class (e.g. Extractor). There is no need for (and indeed
+             confusion can arise if) imports of specific submodules are done.
+"""
+from .quantity import Quantity, ConstantQuantity, AttributeQuantity,\
+    FunctionQuantity, CompiledQuantity, CalculatedQuantity, QuantityFinder
+from .expander import Expander, NullExpander, PadExpander, RepeatExpander,\
+    ModulationExpander, MatrixExpander, CompositeExpander, ShapedExpander,\
+    load_expander_from_hdf5_group
+from .basis import Basis, GramSchmidtBasis, FourierBasis, LegendreBasis,\
+    TrainedBasis, BasisSet, load_basis_from_hdf5_group,\
+    load_basis_set_from_hdf5_group
+from .fitter import Fitter, MetaFitter, Extractor
+from .util import Savable, VariableGrid, TrainingSetIterator,\
+    load_expander_from_hdf5_file, load_basis_from_hdf5_file,\
+    load_basis_set_from_hdf5_file
+
