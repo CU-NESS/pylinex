647a6583e73ff0012d984684dd0e0a3721a6d03b
ktausch <keta6457@colorado.edu>
2019-03-18 22:08:19 -0600

    Supplemented the tests in the axis_expander.py example script showing the use of the AxisExpander class.

--- examples/expander/axis_expander.py
+++ examples/expander/axis_expander.py
@@ -13,14 +13,23 @@ new_axis_position = 1
 new_axis_length = 4
 index = 1
 pad_value = 0
-
 expander = AxisExpander(old_shape, new_axis_position, new_axis_length, index,\
     pad_value=pad_value)
-
 input_array = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 expected_array = np.array([[0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0],\
     [0, 0, 5, 6, 0, 0, 0, 0, 0, 0, 7, 8, 0, 0, 0, 0]])
 output_array = expander(input_array)
+assert(np.all(output_array == expected_array))
 
+old_shape = (4,)
+new_axis_position = 0
+new_axis_length = 4
+index = 0
+pad_value = 0
+expander = AxisExpander(old_shape, new_axis_position, new_axis_length, index,\
+    pad_value=pad_value)
+input_array = np.array([1, 2, 3, 4])
+expected_array = np.array([1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+output_array = expander(input_array)
 assert(np.all(output_array == expected_array))
 
